ecfb1b3011e049221cefd703d414de2273b237c1
Maxime Lareo <maxime@lareo.fr>
2017-04-25 21:42:42 +0200

    init.pp: add comented description

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,28 @@
 #
 # == Parameters:
 #
-# [*Version*]
-#  The version of Docker Compose to be installed, Default is 1.8.1
+# [*version*]
+#   String. Mandatory. Default: 1.8.1
+#   The version of Docker Compose to be installed
 #
-# == Documentation
+# [*docker_compose_path*]
+#   String. Mandatory. Default: /usr/local/bin
+#   The docker-compose binary full path
+#
+# [*compose*]
+#   Hash. Optional. Default: undef
+#   The complete docker-compose.yml file configuration (nested) hash
+#   If an hash is provided here, docker_compose::compose defines are declared with:
+#   create_resources('docker_compose::compose', $compose)
+#
+# == Requires:
+#
+#  puppetlabs-stdlib - https://github.com/puppetlabs/puppetlabs-stdlib
 #
 class docker_compose (
   $compose = {},
+  $version = $::docker_compose::params::version,
+  $docker_compose_path = $::docker_compose::params::docker_compose_path,
 ) inherits docker_compose::params {
 
   require 'stdlib'
